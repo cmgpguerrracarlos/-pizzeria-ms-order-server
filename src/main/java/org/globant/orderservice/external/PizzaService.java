58f3c99f0a1369792ba1cc3d7c860a8b4c5ca559
Carlos Guerra <c.guerra@globant.com>
2022-01-25 21:51:01 -0300

    Fix warnings

--- src/main/java/org/globant/orderservice/external/PizzaService.java
+++ src/main/java/org/globant/orderservice/external/PizzaService.java
@@ -1,8 +1,6 @@
 package org.globant.orderservice.external;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
